8e5769917474d16fe054b0c989bb91faa41b6680
Yuxin Chen <ychen132@terpmail.umd.edu>
2022-12-02 01:24:33 -0500

    keep reading

--- bit.rb
+++ bit.rb
@@ -7,7 +7,7 @@ require 'cgi'
 meta = BEncode.load_file('flatland.torrent') # File or file path
 info_hash = Digest::SHA1.digest(meta["info"].bencode)
 info_hash = CGI.escape(info_hash)
-info_hash = info_hash.downcase
+#info_hash = info_hash.downcase
 
 
 print meta
@@ -18,6 +18,7 @@ print "\n"
 print info_hash
 print "\n"
 hash = "%d4Cz%edh%1c%b0l%5e%cb%cf%2c%7fY%0a%e8%a3%f7%3a%eb"
+print hash
 
 getRequest = "GET /announce?info_hash=#{hash}"
 #should have a randomly generated peer_id
@@ -33,7 +34,7 @@ getRequest += "&numwant=30"
 getRequest += "&key=12345678"
 getRequest += "&compact=1"
 #??? 1 in wireshark for transmission
-getRequest += "&supportcrypto=0"
+getRequest += "&supportcrypto=1"
 getRequest += "&event=started"
 #??? the client ip address?
 getRequest += "&ipv6=2600%3A4040%3A2c69%3Ab500%3Ad32%3Ac436%3Ad7df%3A9ca4"
@@ -45,8 +46,24 @@ cSock = TCPSocket.open("128.8.126.63", 21212)
 cSock.puts(getRequest)
 
 sleep(10)
-#response = cSock.gets
-#print response
+print "\n"
+response = cSock.gets
+print response
+print "\n"
+response = cSock.gets
+print response
+print "\n"
+response = cSock.gets
+print response
+print "\n"
+response = cSock.gets
+print response
+print "\n"
+response += cSock.gets
+print response
+print "\n"
+
+
 cSock.close
 
 
