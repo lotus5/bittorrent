73d82388ad051c20f177fd2c8f72a26a700b8101
Yuxin Chen <ychen132@terpmail.umd.edu>
2022-12-04 03:55:39 -0500

    peer messages done and tested with problems marked in code

--- bit.rb
+++ bit.rb
@@ -4,9 +4,86 @@ require 'socket'
 require 'cgi'
 
 # call the program with ruby bit.rb <filename.torrent> <1 for compact, 0 for noncompact>
-# pieces_hash is a list of hashes for each piece of the file
-# peerState with structure [[socket, am_choking, am_interested, peer_choking, peer_interested]]
-# bitField with length of numPiece, initialized with all 0
+# pieces_hash is an array of hashes for each piece of the file
+# peerState is an array: [[socket, am_choking, am_interested, peer_choking, peer_interested]]
+# bitField is an array with length of numPiece, initialized with all 0
+
+# fields for peerState
+SOCKET = 0
+AMCHOKING = 1
+AMINTERESTED = 2
+PEERCHOKING = 3
+PEERINTERESTED = 4
+
+# message functions for peer communication
+# all functions have the return value format of [message, length_of_message]
+def keepAliveMessage                            # <len=0000>
+    ["\x00\x00\x00\x00", 4]
+end
+
+def chokeMessage                                # <len=0001><id=0>
+    ["\x00\x00\x00\x01\x00", 5]
+end
+
+def unchokeMessage                              # <len=0001><id=1>
+    ["\x00\x00\x00\x01\x01", 5]
+end
+
+def interestedMessage                           # <len=0001><id=2>
+    ["\x00\x00\x00\x01\x02", 5]
+end
+
+def notInterestedMessage                        # <len=0001><id=3>
+    ["\x00\x00\x00\x01\x03", 5]
+end
+# TODO: weird performance with length in send
+def haveMessage(id)                             # <len=0005><id=4><piece index>
+    id = [id].pack('N')
+    m_have = "\x00\x00\x00\x05\x04#{id}"
+    [m_have, 9]     #----> should have length of 9(len:4 + id:1 + index:4)
+    [m_have, 16]    #----> the send socket won't take anything under 16 as length for sending this message.
+end
+
+def bitfieldMessage(bitArray)                   # <len=0001+X><id=5><bitfield>
+    len = (bitArray.length.to_f/8).ceil + 1
+    m_bitfield = "#{[len].pack('N')}\x05"
+    #pad the bitfield with 0 at the end
+    numPad = 8 - bitArray.length.modulo(8)
+    b = bitArray.join()
+    for i in 1..numPad
+        b += "0"
+    end
+    b = b.to_s.scan(/.{8}/)
+    for i in 0..(b.length - 1)
+        tmp = [b[i].to_i(2)]
+        m_bitfield += tmp.pack('C')
+    end
+    [m_bitfield, (len + 4)]
+end
+
+def requestMessage(id, beg, len)                # <len=0013><id=6><index><begin><length>
+    id = [id].pack('N')
+    beg = [beg].pack('N')
+    len = [len].pack('N')
+    m_req = "\x00\x00\x00\x0d\x06#{id}#{beg}#{len}"
+    [m_req, 17]
+end
+
+# TODO: pieceMessage
+
+def cancelMessage(id, beg, len)                 # <len=0013><id=8><index><begin><length>
+    id = [id].pack('N')
+    beg = [beg].pack('N')
+    len = [len].pack('N')
+    m_req = "\x00\x00\x00\x0d\x08#{id}#{beg}#{len}"
+    [m_req, 17]
+end
+
+def portMessage(port)                           # <len=0003><id=9><listen-port>
+    port = [port].pack('n')
+    m_port = "\x00\x00\x00\x03\x09#{port}"
+    [m_port, 7]
+end
 
 def generatePeerID()
     x = [*'a'..'z', *'A'..'Z', *0..9]
@@ -15,30 +92,20 @@ def generatePeerID()
     x.join
 end
 
-SOCKET = 0
-AMCHOKING = 1
-AMINTERESTED = 2
-PEERCHOKING = 3
-PEERINTERESTED = 4
-
-#Parsing the torrent file
+# Parsing the torrent file
 meta = BEncode.load_file(ARGV[0])
 compact = ARGV[1]
 info_hash = Digest::SHA1.digest(meta["info"].bencode)
 info_hash = CGI.escape(info_hash)
 
-
-#print meta["info"]
-
-
-#properties of the file
+# Properties of the file
 name = meta["info"]["name"]
 totalLen = meta["info"]["length"]
 pieceLen = meta["info"]["piece length"]
 numPiece = ((totalLen.to_f)/(pieceLen.to_f)).ceil
 bitField = Array.new(numPiece, 0)
 
-#the hashes for each piece
+# Hashes for each piece
 pieces_hash = meta["info"]["pieces"].scan(/.{20}/)
 for i in 0..(pieces_hash.length - 1) do
     pieces_hash[i] = pieces_hash[i].unpack('H*')
@@ -49,66 +116,59 @@ print "\ninfo_hash for file is: #{info_hash}\n\n"
 print "There are #{numPiece} pieces with piece length #{pieceLen}\n"
 print "\npieces_hash for each piece is: #{pieces_hash}\n\n"
 
-#tracker communication
-#forming the GET request
+trackerInfo = meta["announce"].split("/")
+trackerInfo = trackerInfo[2].split(":")
+
+# Creating connection
+cSock = TCPSocket.open(trackerInfo[0], trackerInfo[1])
+
+# Tracker communication
+# Forming the GET request
 getRequest = "GET /announce?info_hash=#{info_hash}"
 peer_id = generatePeerID()
 #p peer_id
 getRequest += "&peer_id=#{CGI.escape(peer_id)}"
-#TODO: port???
-getRequest += "&port=51413"
+# our port
+getRequest += "&port=#{cSock.addr[1]}"
 getRequest += "&uploaded=0"
 getRequest += "&downloaded=0"
-#for multi-file mode
-#length = meta["info"]["files"][0]["length"]
+# for multi-file mode
+# length = meta["info"]["files"][0]["length"]
 length = totalLen
 getRequest += "&left=#{length}"
-#bobby said 30
+# Bobby said 30
 getRequest += "&numwant=30"
-# *********************************** compact ******************************************
 getRequest += "&compact=#{compact}"
 getRequest += "&event=started"
-#http and traker ip and port
-trackerInfo = meta["announce"].split("/")
-trackerInfo = trackerInfo[2].split(":")
+# Http and traker ip and port
 getRequest += " HTTP/1.1\r\nHost:#{trackerInfo[0]}:#{trackerInfo[1]}\r\n\r\n"
 
-#creating connection
-cSock = TCPSocket.open(trackerInfo[0], trackerInfo[1])
-#sending GET request
+# Sending GET request
 cSock.puts(getRequest)
 
-#receiving header for tracker response
+# Receiving header for tracker response
 respHeader = cSock.gets("\r\n\r\n")
 #print "tracker response header: \n"
 #print respHeader
 #print "\n"
 
-#extracting length of the bencode from the header
+# Extracting length of the bencode from the header
 benLen = respHeader.scan(/Content-Length: \d+/)
 benLen = benLen[0].scan(/\d+/)
 benLen = benLen[0].to_i
 
-#reading bencode from socket based on length in header
+# Reading bencode from socket based on length in header
 bencode = cSock.read(benLen)
 data = BEncode.load(bencode)
-
-#print data
-#print "\n"
-#print "\n"
-
-
-
 #print data
 #print "\n"
 #print numPeers
 #print "\n"
 
-#parsing peers
-
+# Parsing peers
 peerInfo = []
 if data["peers"].is_a?(String)
-    #compact response
+    # compact response
     peerInfo = data["peers"].bytes.each_slice(6).to_a
     numPeers = data["peers"].length/6
     for i in 0..(numPeers - 1) do
@@ -122,7 +182,7 @@ if data["peers"].is_a?(String)
         peerInfo[i] = peerIp + ":" + peerPort
     end
 else
-    #noncompact response
+    # noncompact response
     for i in 0..30 do
         if data["peers"][i] == nil
             break
@@ -138,16 +198,13 @@ print peerInfo
 print "\n\n"
 
 #TODO: Peer communication
-#connecting to all peers
+# Connecting to all peers
 peerState = Array.new(numPeers){Array.new(5)}
 
-#peerSock = TCPSocket.open("128.8.126.63", "51413")
 #print "connected\n"
 hex_info_hash = Digest::SHA1.digest(meta["info"].bencode)
 handshake = "\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00#{hex_info_hash}#{peer_id}"
-p handshake
 for i in 0..(numPeers - 1) do
-    #state for peers
 =begin
     This is how I think we should eventually connect to peers, this handles the case where the connection
     is refused w/o our client crashing
@@ -172,7 +229,7 @@ for i in 0..(numPeers - 1) do
         s = TCPSocket.open(x[0], x[1])
         p "connected to Poole client"
         s.send(handshake, handshake.length)
-        p "sent handshake"
+        p "handshake sent"
         s.gets()
     else
         #p "not the Poole client, ignoring"
@@ -187,9 +244,11 @@ for i in 0..(numPeers - 1) do
     #peerAddr = peerInfo[i].split(":")
     #pSock[i] = TCPSocket.open(peerAddr[0], peerAddr[1])
 end
-
 #print peerState
 #print "\n"
 
+#whenever a client connects we send a biffield message (if we have a piece)
+
+sleep(3)
 cSock.close
 
