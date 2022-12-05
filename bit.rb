cea0e734ded9e092255bc461d4c77a334ca43cbc
Yuxin Chen <ychen132@terpmail.umd.edu>
2022-12-04 22:43:41 -0500

    minor fix

--- bit.rb
+++ bit.rb
@@ -18,30 +18,29 @@ PEERINTERESTED = 4
 # message functions for peer communication
 # all functions have the return value format of [message, length_of_message]
 def keepAliveMessage                            # <len=0000>
-    ["\x00\x00\x00\x00", 4]
+    "\x00\x00\x00\x00"
 end
 
 def chokeMessage                                # <len=0001><id=0>
-    ["\x00\x00\x00\x01\x00", 5]
+    "\x00\x00\x00\x01\x00"
 end
 
 def unchokeMessage                              # <len=0001><id=1>
-    ["\x00\x00\x00\x01\x01", 5]
+    "\x00\x00\x00\x01\x01"
 end
 
 def interestedMessage                           # <len=0001><id=2>
-    ["\x00\x00\x00\x01\x02", 5]
+    "\x00\x00\x00\x01\x02"
 end
 
 def notInterestedMessage                        # <len=0001><id=3>
-    ["\x00\x00\x00\x01\x03", 5]
+    "\x00\x00\x00\x01\x03"
 end
 # TODO: weird performance with length in send
 def haveMessage(id)                             # <len=0005><id=4><piece index>
     id = [id].pack('N')
     m_have = "\x00\x00\x00\x05\x04#{id}"
-    [m_have, 9]     #----> should have length of 9(len:4 + id:1 + index:4)
-    #[m_have, 16]    #----> the send socket won't take anything under 16 as length for sending this message.
+    m_have
 end
 
 def bitfieldMessage(bitArray)                   # <len=0001+X><id=5><bitfield>
@@ -58,7 +57,7 @@ def bitfieldMessage(bitArray)                   # <len=0001+X><id=5><bitfield>
         tmp = [b[i].to_i(2)]
         m_bitfield += tmp.pack('C')
     end
-    [m_bitfield, (len + 4)]
+    m_bitfield
 end
 
 def requestMessage(id, beg, len)                # <len=0013><id=6><index><begin><length>
@@ -66,7 +65,7 @@ def requestMessage(id, beg, len)                # <len=0013><id=6><index><begin>
     beg = [beg].pack('N')
     len = [len].pack('N')
     m_req = "\x00\x00\x00\x0d\x06#{id}#{beg}#{len}"
-    [m_req, 17]
+    m_req
 end
 
 # TODO: pieceMessage
@@ -75,14 +74,14 @@ def cancelMessage(id, beg, len)                 # <len=0013><id=8><index><begin>
     id = [id].pack('N')
     beg = [beg].pack('N')
     len = [len].pack('N')
-    m_req = "\x00\x00\x00\x0d\x08#{id}#{beg}#{len}"
-    [m_req, 17]
+    m_cancel = "\x00\x00\x00\x0d\x08#{id}#{beg}#{len}"
+    m_cancel
 end
 
 def portMessage(port)                           # <len=0003><id=9><listen-port>
     port = [port].pack('n')
     m_port = "\x00\x00\x00\x03\x09#{port}"
-    [m_port, 7]
+    m_port
 end
 
 def generatePeerID()
@@ -289,8 +288,8 @@ for i in 0..(numPeers - 1) do
         x = peerInfo[i].split(":")
         s = TCPSocket.open(x[0], x[1])
         p "connected to Poole client"
-        s.write(handshake, handshake.length)
-        p "handshake sent"
+        s.write(handshake)
+        p "handshake sent #{handshake.length}"
 
         # parsing the received handshake
         pstrlen = s.read(1).unpack('C')
@@ -303,7 +302,7 @@ for i in 0..(numPeers - 1) do
         # testing for sending messages
         #test = haveMessage(1)
         #p test
-        #s.write(test[0], test[1])
+        #s.write(test)
     else
         #p "not the Poole client, ignoring"
         s = 9999
@@ -330,7 +329,9 @@ while test == -1
   test = server.accept    # Wait for a client to connect
 end
 
-parseResponse(test)
+while 1 do
+    parseResponse(test)
+end
 
 test.close
 =end
--- test.rb
+++ test.rb
@@ -7,30 +7,29 @@ require 'cgi'
 # First run bit.rb, then run this program as ruby bit.rb 2000
 
 def keepAliveMessage                            # <len=0000>
-    ["\x00\x00\x00\x00", 4]
+    "\x00\x00\x00\x00"
 end
 
 def chokeMessage                                # <len=0001><id=0>
-    ["\x00\x00\x00\x01\x00", 5]
+    "\x00\x00\x00\x01\x00"
 end
 
 def unchokeMessage                              # <len=0001><id=1>
-    ["\x00\x00\x00\x01\x01", 5]
+    "\x00\x00\x00\x01\x01"
 end
 
 def interestedMessage                           # <len=0001><id=2>
-    ["\x00\x00\x00\x01\x02", 5]
+    "\x00\x00\x00\x01\x02"
 end
 
 def notInterestedMessage                        # <len=0001><id=3>
-    ["\x00\x00\x00\x01\x03", 5]
+    "\x00\x00\x00\x01\x03"
 end
 # TODO: weird performance with length in send
 def haveMessage(id)                             # <len=0005><id=4><piece index>
     id = [id].pack('N')
     m_have = "\x00\x00\x00\x05\x04#{id}"
-    [m_have, 9]     #----> should have length of 9(len:4 + id:1 + index:4)
-    #[m_have, 16]    #----> the send socket won't take anything under 16 as length for sending this message.
+    m_have
 end
 
 def bitfieldMessage(bitArray)                   # <len=0001+X><id=5><bitfield>
@@ -47,7 +46,7 @@ def bitfieldMessage(bitArray)                   # <len=0001+X><id=5><bitfield>
         tmp = [b[i].to_i(2)]
         m_bitfield += tmp.pack('C')
     end
-    [m_bitfield, (len + 4)]
+    m_bitfield
 end
 
 def requestMessage(id, beg, len)                # <len=0013><id=6><index><begin><length>
@@ -55,7 +54,7 @@ def requestMessage(id, beg, len)                # <len=0013><id=6><index><begin>
     beg = [beg].pack('N')
     len = [len].pack('N')
     m_req = "\x00\x00\x00\x0d\x06#{id}#{beg}#{len}"
-    [m_req, 17]
+    m_req
 end
 
 # TODO: pieceMessage
@@ -64,14 +63,14 @@ def cancelMessage(id, beg, len)                 # <len=0013><id=8><index><begin>
     id = [id].pack('N')
     beg = [beg].pack('N')
     len = [len].pack('N')
-    m_req = "\x00\x00\x00\x0d\x08#{id}#{beg}#{len}"
-    [m_req, 17]
+    m_cancel = "\x00\x00\x00\x0d\x08#{id}#{beg}#{len}"
+    m_cancel
 end
 
 def portMessage(port)                           # <len=0003><id=9><listen-port>
     port = [port].pack('n')
     m_port = "\x00\x00\x00\x03\x09#{port}"
-    [m_port, 7]
+    m_port
 end
 
 port = ARGV[0]
@@ -79,8 +78,9 @@ port = ARGV[0]
 cSock = TCPSocket.open("127.0.0.1", port)
 
 test = bitfieldMessage([1,1,1,1,1,1,1,1,1])
-#test = portMessage(2000)
-cSock.write(test[0], test[1])
+cSock.write(test)
+test = portMessage(2000)
+cSock.write(test)
 
 cSock.close
 
